2454cf843db39701e9ed0c8669bd80133838a8e6
SaltPepperSalt <dksro324@gmail.com>
2020-06-09 08:14:54 +0900

    feat : init main index.html

---
+++ js/modal.js
@@ -0,0 +1,49 @@
+const recipes = [{
+  id: 1,
+  name: 'Boiled egg',
+  ingredients: ['egg', 'water', 'salt'],
+  ingredientsAmounts: [2, 600, 10],
+  ingredientsUnits: ['개', 'ml', 'g'],
+  content: '삶은 달걀',
+  imgSrc: "./images/boiledegg.jpg"
+}, {
+  id: 2,
+  name: 'Steak',
+  ingredients: ['egg', 'water', 'salt'],
+  ingredientsAmounts: [2, 600, 10],
+  ingredientsUnits: ['개', 'ml', 'g'],
+  content: '삶은 달걀',
+  imgSrc: "./images/steak.jpg"
+}, {
+  id: 3,
+  name: 'Beef',
+  ingredients: ['egg', 'water', 'salt'],
+  ingredientsAmounts: [2, 600, 10],
+  ingredientsUnits: ['개', 'ml', 'g'],
+  content: '삶은 달걀',
+  imgSrc: "./images/beef.jpg"
+}, {
+  id: 4,
+  name: 'Spaghetti',
+  ingredients: ['egg', 'water', 'salt'],
+  ingredientsAmounts: [2, 600, 10],
+  ingredientsUnits: ['개', 'ml', 'g'],
+  content: '삶은 달걀',
+  imgSrc: "./images/spa.jpg"
+}, {
+  id: 5,
+  name: 'Lasagna',
+  ingredients: ['egg', 'water', 'salt'],
+  ingredientsAmounts: [2, 600, 10],
+  ingredientsUnits: ['개', 'ml', 'g'],
+  content: '삶은 달걀',
+  imgSrc: "./images/lasagna.jpg"
+}, {
+  id: 6,
+  name: 'Pancake',
+  ingredients: ['egg', 'water', 'salt'],
+  ingredientsAmounts: [2, 600, 10],
+  ingredientsUnits: ['개', 'ml', 'g'],
+  content: '삶은 달걀',
+  imgSrc: "./images/pancake.jpg"
+}];
